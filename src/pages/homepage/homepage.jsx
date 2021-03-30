45059b3ca1a203a3f0d5843f7e3d279b57246687
Mohamed Ali Trabelsi <52967116+Mukkun-san@users.noreply.github.com>
2021-03-30 03:23:09 +0100

    Update homepage.jsx

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Homepage = () => {
-    return ( <h2 class="mx-auto"><a href="https://skywaytour.netlify.app/">Visit https://skywaytour.netlify.app</a></h2> );
+    return ( <br><h2 class="mx-auto"><a href="https://skywaytour.netlify.app/">Visit https://skywaytour.netlify.app</a></h2> );
 }
  
 export default Homepage;
